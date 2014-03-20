e5ce7ed2ba87b711461eb24845a217cc7ace3560
Fachrul Pralienka BM <beeboy_pralienka@rocketmail.com>
2014-03-20 12:57:22 +0700

    Perubahan testing yang ketiga ya

--- test/medrecappx/Service/A_SpesialisServiceTest.java
+++ test/medrecappx/Service/A_SpesialisServiceTest.java
@@ -70,7 +70,7 @@ public class A_SpesialisServiceTest {
         System.out.println("1. serviceInsertSpesialis");
 
         ExpIdSpesialis = "Sp.Mt";
-        ExpNmSpesialis = "Spesialis empat mata";
+        ExpNmSpesialis = "Spesialis mata hati";
         ExpTarifKonsul = 70000;
         
         sp.setIdSpesialis(ExpIdSpesialis);
